12ce0faba3b9abe59029b1bfe332a3e0eb3040c2
ljj <sumkol123@gmail.com>
2024-09-26 19:13:07 +0900

    Create New File Mage.cs , Warrior.cs why : Code Location Change

--- TextRPG_Team_Project/Unit/Character/Character.cs
+++ TextRPG_Team_Project/Unit/Character/Character.cs
@@ -85,28 +85,9 @@ namespace TextRPG_Team_Project
     }
 
    
-    public class Warrior : Character // 워리어 클래스
-    {
-        public Warrior(String _name , int _level , int _health, int _attack, int _defense) : base(_name , _level , _health , _attack , _defense) // 전사 생성시 초기값 설정
-        {
+  
 
-
-        }
-
-
-
-    }
-
-    public class Mage : Character // 마법사 클래스
-    {
-        public Mage(String _name, int _level, int _health, int _attack, int _defense) : base(_name, _level, _health, _attack, _defense)
-        {
-
-
-
-        }
-
-    }
+   
     
 
 
---
+++ TextRPG_Team_Project/Unit/Character/Mage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team_Project
+{
+    public class Mage : Character // 마법사 클래스
+    {
+        public Mage(String _name, int _level, int _health, int _attack, int _defense) : base(_name, _level, _health, _attack, _defense)
+        {
+
+
+
+        }
+
+    }
+}
