cc2a211e6484963bded601b993b65c386a323fe7
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-09-26 16:56:40 +0900

    Update TakeDamage, Add Attack
    피격 데미지 처리 업데이트, 공격 추가

--- TextRPG_Team_Project/Unit/Monster/Monster.cs
+++ TextRPG_Team_Project/Unit/Monster/Monster.cs
@@ -1,7 +1,11 @@
 ﻿namespace TextRPG_Team_Project
 {
+    public delegate void MonsterAttackHandler(int damage);
+
     public class Monster : IUnit
     {
+        public event MonsterAttackHandler? OnAttack;
+
         private string? _name;
         private int _level;
         private int _health;
@@ -13,14 +17,39 @@
         public string Name { get { return _name ?? "Unknown"; } private set { _name = value; } }
         public int Level { get => _level; private set => _level = value; }
         public int Health { get => _health; private set => _health = value; }
-        public int MaxHealth { get => _maxHealth; private set => _maxHealth = value; }
+        public int MaxHealth { get => _maxHealth; }
         public int Attack { get => _attack; private set => _attack = value; }
         public int Defense { get => _defense; private set => _defense = value; }
         public bool IsDead { get => _isDead; private set => _isDead = value; }
 
         public void TakeDamage(int damage)
         {
+            int error = (int)Math.Ceiling(damage * 0.1f); // 소수 첫번째 자리 올림
 
+            Random random = new Random();
+            int totalDamage = random.Next(damage - error, damage + error + 1);
+
+            _health -= totalDamage;
+
+            if (_health < 0)
+            {
+                _health = 0;
+                _isDead = true;
+            }
+        }
+
+        public void MonsterAttack(int damage)
+        {
+            OnAttack?.Invoke(damage);
+        }
+
+        public void Recovery()
+        {
+            if(_isDead)
+            {
+                _health = _maxHealth;
+                _isDead = false;
+            }
         }
     }
 }
