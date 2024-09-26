0780d51d4487ba4cee2631b6e9ed7aac61d5dd3f
1suyb <suyb1234@gmail.com>
2024-09-26 16:18:11 +0900

    <feat> Add SceneClass and three scenes

---
+++ TextRPG_Team_Project/Scene/Scene.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team_Project.Scene
+{
+	public abstract class Scene
+	{
+
+		public abstract int PlayScene();
+
+		public void DisplayIntro(string name) 
+		{
+			Console.Clear();
+			Console.WriteLine("[name]");
+		}
+		public void DisplayGetInputNumber()
+		{
+			Console.WriteLine("원하시는 행동을 입력해주세요.");
+			Console.Write(">>>   ");
+		}
+		public void DisplayGetInputString(string setting)
+		{
+			Console.WriteLine($"원하시는 {setting}을 입력해주세요.");
+			Console.Write(">>>   ");
+		}
+	}
+	public class StartScene : Scene
+	{
+		public  void DisplayInitScene()
+		{
+			DisplayIntro("게임 이름") // 게임 이름 뭐로할까용
+			Console.WriteLine("어서오세요");
+			Console.WriteLine("");
+			Console.WriteLine("1. 시작하기");
+		}
+		public override int PlayScene()
+		{
+			DisplayInitScene();
+			return TempGameManager.GetNumberInput(1, 2);
+		}
+	}
+
+	public class HomeScene : Scene
+	{
+		public void DisplayInitScene()
+		{
+			DisplayIntro("마을");
+			Console.WriteLine("");
+			Console.WriteLine("1. 상태보기");
+			Console.WriteLine("2. 전투");
+			DisplayGetInputNumber();
+		}
+
+		public override int PlayScene()
+		{
+			DisplayInitScene();
+			return TempGameManager.GetNumberInput(1,3);
+		}
+	}
+
+	public class StatusScene : Scene
+	{
+		event Func<string> UserStatusInfo;  // 유저 Status를 보여주는 string을 반환하는 함수를 할당해주세요
+		public void DisplayInitScene(string status)
+		{
+			DisplayIntro("상태보기");
+			Console.WriteLine();
+			Console.WriteLine(status);
+
+			Console.WriteLine("0. 나가기");
+			DisplayGetInputNumber();
+		}
+
+		public override int PlayScene()
+		{
+			DisplayInitScene(UserStatusInfo.Invoke());
+			return TempGameManager.GetNumberInput(0, 1);
+		}
+	}
+
+	public class DebugScene : Scene
+	{
+		event Func<string> DebugInfo;
+		public void DisplayInitScene(string strings)
+		{
+			DisplayIntro("Debug");
+			Console.WriteLine();
+			Console.WriteLine(strings);
+
+			Console.WriteLine("0. 나가기");
+			DisplayGetInputNumber();
+		}
+
+		public override int PlayScene()
+		{
+			DisplayInitScene(DebugInfo());
+			return TempGameManager.GetNumberInput(0, 1);
+		}
+	}
+
+
+}
---
+++ TextRPG_Team_Project/Scene/TempGameManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team_Project.Scene
+{
+	public class TempGameManager
+	{
+		enum GameStatus
+		{
+			Start=0,
+			Home,
+			Status,
+
+		}
+		private GameStatus _status = GameStatus.Start;
+
+		private Scene _startScene;
+		private Scene _homeScene;
+		private Scene _statusScene;
+
+		public TempGameManager()
+		{
+			_startScene = new StartScene();
+			_homeScene = new HomeScene();
+			_statusScene = new StartScene();
+		}
+
+		public void GameMain()
+			/*
+			 디버깅을 위한 임시 게임메인
+			 */
+		{
+			while (true)
+			{
+				switch (_status)
+				{
+					case GameStatus.Start:
+						_status = (GameStatus)_startScene.PlayScene();
+						break;
+					case GameStatus.Home:
+						_status = (GameStatus)_homeScene.PlayScene();
+						break;
+					case GameStatus.Status:
+						_status = (GameStatus)_statusScene.PlayScene();
+						break;
+				}
+			}
+		}
+		public static int GetNumberInput(int min, int max)
+			/*사용자의 숫자입력을 받는 함수
+			 min : 입력가능한 최소값 (포함)
+			 max : 입력가능한 최대값 (미포함)
+			*/
+		{
+			string? input = "";
+			int inputNumber = 0;
+			
+			while (input == null || input == "")
+			{
+				input = Console.ReadLine();
+				if (int.TryParse(input, out inputNumber)) 
+				{
+					if (min <= inputNumber && inputNumber < max) { return inputNumber; }
+				}
+				else { input = ""; }
+			}
+			return -1;
+		}
+	}
+}
