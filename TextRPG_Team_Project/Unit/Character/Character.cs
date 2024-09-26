1ad25ef4e373810b05b58690df6cc01ec5a0117b
ljj <sumkol123@gmail.com>
2024-09-26 16:58:30 +0900

    Add Character Class

---
+++ TextRPG_Team_Project/Unit/Character/Character.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using TextRPG_Team_Project;
+namespace TextRPG_Team_Project
+{
+
+    public class Character : IUnit
+    {
+
+        public string Name { get; }
+        public int Level { get; }
+        public int Health { get; }
+        public int Attack { get; }
+        public int Defense { get; }
+        public bool IsDead { get; }
+
+       
+        public void TakeDamage(int damage)
+        {
+            
+
+        }
+
+        
+
+        public Character()
+        {
+
+            
+
+
+
+        }
+
+
+
+     
+
+
+
+
+    }
+   
+    
+    
+
+
+
+}
