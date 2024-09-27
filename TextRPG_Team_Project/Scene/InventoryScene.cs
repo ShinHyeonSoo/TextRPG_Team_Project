bf4a4f85a1aef1c433fc89fff96feab36d4d1f02
Jooglorystar <121430174+Jooglorystar@users.noreply.github.com>
2024-09-27 14:45:31 +0900

    [Return] InventoryScene.cs
    develop의 내용으로 덮어 씌움

--- TextRPG_Team_Project/Scene/InventoryScene.cs
+++ TextRPG_Team_Project/Scene/InventoryScene.cs
@@ -13,19 +13,9 @@ namespace TextRPG_Team_Project.Scene
 {
     public class InventoryScene : Scene
     {
-        // 이 이하는 임시코드
-        Character character = DataManager.Instance().GetPlayer();
-        Shop shop = new Shop();
-        // 임시코드 끝
         // 인벤토리를 받거나
         // 인벤토리의 함수들을 받기
-        enum InventoryState
-        {
-            Start = 0,
-            ManagingEuipment = 1,
-            UseItem = 2,
-            Shop = 3
-        }
+
         private Defines.InventoryState _state;
 
         public InventoryScene() { }
@@ -35,12 +25,7 @@ namespace TextRPG_Team_Project.Scene
             DisplayIntro("인벤토리");
             Console.WriteLine();
             Console.WriteLine("인벤토리 목록 출력");
-
-            shop.DisplayWeaponShopList();
-            shop.DisplayArmorShopList();
-            shop.DisplayPotionShopList();
-
-            DisplayOption(new List<string>() { "1. 장착 관리", "2. 아이템 사용", "3. 아이템 구입/판매" });
+            DisplayOption(new List<string>() { "장착 관리", "아이템 사용" });
             Console.WriteLine("0. 나가기");
             DisplayGetInputNumber();
         }
@@ -59,48 +44,6 @@ namespace TextRPG_Team_Project.Scene
             DisplayIntro("인벤토리");
             Console.WriteLine();
             Console.WriteLine("인벤토리 사용아이템 목록 출력");
-            Console.WriteLine();
-            Console.WriteLine("0. 나가기");
-            DisplayGetInputNumber();
-        }
-
-        public void DisplayShop()
-        {
-            DisplayIntro("상점");
-            Console.WriteLine();
-            DisplayOption(new List<string>() { "1. 무기 상점", "2. 방어구 상점", "3. 물약 상점" });
-            Console.WriteLine();
-            Console.WriteLine("0. 나가기");
-            DisplayGetInputNumber();
-        }
-
-        public void DisplayWeaponShop()
-        {
-            DisplayIntro("무기 상점");
-            Console.WriteLine();
-            Console.WriteLine("무기 리스트");
-            DisplayOption(new List<string>() { "1.", "2.", "3." });
-            Console.WriteLine();
-            Console.WriteLine("0. 나가기");
-            DisplayGetInputNumber();
-        }
-
-        public void DisplayArmorShop()
-        {
-            DisplayIntro("방어구 상점");
-            Console.WriteLine();
-            Console.WriteLine("방어구 리스트");
-            DisplayOption(new List<string>() { "1.", "2.", "3." });
-            Console.WriteLine();
-            Console.WriteLine("0. 나가기");
-            DisplayGetInputNumber();
-        }
-
-        public void DisplayPotionShop()
-        {
-            DisplayIntro("물약 상점");
-            Console.WriteLine();
-            DisplayOption(new List<string>() { "물약 리스트" });
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
             DisplayGetInputNumber();
