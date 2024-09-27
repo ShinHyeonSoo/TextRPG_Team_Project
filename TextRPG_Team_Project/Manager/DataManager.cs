0ed1acd994212cbd80dbee8a2e9a4d3fe16a2025
ljj <sumkol123@gmail.com>
2024-09-27 20:37:06 +0900

    [Fix]DataManager.cs

--- TextRPG_Team_Project/Manager/DataManager.cs
+++ TextRPG_Team_Project/Manager/DataManager.cs
@@ -23,7 +23,7 @@ namespace TextRPG_Team_Project
         public Character GetPlayer() 
         {
             if (_player == null)
-                return new Character("홍길동", 1,100,1,1);
+                return new Warrior("홍길동", 1,100,1,1);
 
             return _player; 
         }
