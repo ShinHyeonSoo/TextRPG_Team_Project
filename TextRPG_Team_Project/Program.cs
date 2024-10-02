7b09a89c0d7a2b5b9b677351a6223be70a25f3a7
1suyb <suyb1234@gmail.com>
2024-10-02 12:47:59 +0900

    refactor 게임매니저 싱글톤 패턴 수정

--- TextRPG_Team_Project/Program.cs
+++ TextRPG_Team_Project/Program.cs
@@ -1,12 +1,10 @@
 ﻿using TextRPG_Team_Project.Scene;
-
 namespace TextRPG_Team_Project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            GameManager gameManager = new GameManager();
 			GameManager.Instance.GameMain();
         }
     }
